2ad21c905cf7449f55452adefe29eb6e30f1abfa
George <george.dickinson@gmail.com>
2018-02-19 23:55:36 -0700

    added ability to win

--- script.js
+++ script.js
@@ -132,6 +132,8 @@ class PieceList {
         this.availableMovesForCurrentPiece = '';
 
         this.gameRecord = [];
+        this.gameOver = false;
+        this.winner = "None";
 
 
     }
@@ -1000,6 +1002,33 @@ class PieceList {
         return;
     }
 
+    isGameWon() {
+        //TODO     
+        if (this.blackKing.length < 2){
+            this.gameOver = true;
+            this.winner = "White"; 
+            return true;
+        }
+        
+        if (this.whiteKing.length < 2){
+            this.gameOver = true;
+            this.winner = "Black";
+            return true;
+            
+        }
+        return false;
+    }
+
+    //Basic AI///
+
+    getAllMovesForTurn() {
+        //TODO
+        return;
+    }
+
+
+
+
 } ///////////////end of class//////////////////////////////////////////////
 
 
@@ -1126,78 +1155,51 @@ function updateMessageBox(x, y) {
     info.innerHTML = 'Side to move: ' + game.sideToMove + '<br />Position X: ' + x + '<br />Position Y: ' + y + '<br />Piece: ' + game.getHexContent(x, y);
 }
 
+function displayWinnerMessage() {
+    info.innerHTML = game.winner + ' Wins!';
+}
+
 function selectPiece(p) {
-    var rect = c.getBoundingClientRect();
-    var xPos = (p.pageX - rect.left);
-    var yPos = (p.pageY - rect.top);
-    
-    var can_height = ctx.canvas.height;
-    var can_width = ctx.canvas.width;
-    
-    var yAdjust = can_height/45;
-    var xAdjust = can_width/25;
-    
+    if (game.gameOver == false) {
+        var rect = c.getBoundingClientRect();
+        var xPos = (p.pageX - rect.left);
+        var yPos = (p.pageY - rect.top);
 
-    if (xPos < (xAdjust*0.1) || yPos < (yAdjust*1.6) || xPos > can_width - xAdjust || yPos > can_height - (yAdjust*4)) {
-        info.innerHTML = 'Off Board';
-    } else {
-        if ((yPos % yAdjust) < (yAdjust * 0.5)) {
-            //console.log("YPos: " + yPos % yAdjust);
-            yPos = Math.floor((yPos/yAdjust)-1);
+        var can_height = ctx.canvas.height;
+        var can_width = ctx.canvas.width;
+
+        var yAdjust = can_height / 45;
+        var xAdjust = can_width / 25;
+
+
+        if (xPos < (xAdjust * 0.1) || yPos < (yAdjust * 1.6) || xPos > can_width - xAdjust || yPos > can_height - (yAdjust * 4)) {
+            info.innerHTML = 'Off Board';
         } else {
-           //console.log("YPos: " + yPos % yAdjust);
-            yPos = Math.ceil((yPos/yAdjust)-1);
-        }
-        if ((xPos % xAdjust ) < (xAdjust * 0.5)) {
-            //console.log("XPos: "+xPos % xAdjust);
-            xPos = Math.floor(xPos / xAdjust / 2);
-        } else {
-            //console.log("XPos: "+xPos % xAdjust);
-            xPos = Math.ceil((xPos / xAdjust )/2);
-        }
-
-        updateMessageBox(xPos, yPos)
-
-        board.drawBoard();
-        game.setPieces();
-
-        //check if piece is selected to move  
-        if (game.pieceToMoveHexSelected != true) {
-
-            //check if hex selected contains a valid piece
-            if (game.availableForSelection(xPos, yPos, game.sideToMove)) {
-                game.selectHex(xPos, yPos, 'blue');
-                game.setPieceSelected(xPos, yPos);
-                game.showHexsAvailabletoMove(xPos, yPos, game.pieceSelected);
-                game.oldX = game.selectedCellX;
-                game.oldY = game.selectedCellY;
-                game.readyToMove = true;
-                game.pieceToMoveHexSelected = true;
-
-                updateMessageBox(xPos, yPos);
+            if ((yPos % yAdjust) < (yAdjust * 0.5)) {
+                //console.log("YPos: " + yPos % yAdjust);
+                yPos = Math.floor((yPos / yAdjust) - 1);
+            } else {
+                //console.log("YPos: " + yPos % yAdjust);
+                yPos = Math.ceil((yPos / yAdjust) - 1);
+            }
+            if ((xPos % xAdjust) < (xAdjust * 0.5)) {
+                //console.log("XPos: "+xPos % xAdjust);
+                xPos = Math.floor(xPos / xAdjust / 2);
+            } else {
+                //console.log("XPos: "+xPos % xAdjust);
+                xPos = Math.ceil((xPos / xAdjust) / 2);
             }
 
-        } else {
-            //check if hex to move to is valid
+            updateMessageBox(xPos, yPos)
 
-            if (game.availableForMove(xPos, yPos) && game.availableCurrentPieceMove(xPos, yPos)) {
-                //move piece
-                game.movePiece(xPos, yPos, game.pieceSelected);
-                game.readyToMove = false;
-                game.pieceToMoveHexSelected = false;
-                game.changeSideToMove();
-                game.resetAvailableMoves();
-                game.oldX = 15;
-                game.oldY = 40;
+            board.drawBoard();
+            game.setPieces();
 
-                updateMessageBox(xPos, yPos);
+            //check if piece is selected to move  
+            if (game.pieceToMoveHexSelected != true) {
 
-                return; //TODO
-            } else {
-                game.resetAvailableMoves();
-                //if hex not available unselect piece - select new piece if one in hex
+                //check if hex selected contains a valid piece
                 if (game.availableForSelection(xPos, yPos, game.sideToMove)) {
-                    game.resetAvailableMoves();
                     game.selectHex(xPos, yPos, 'blue');
                     game.setPieceSelected(xPos, yPos);
                     game.showHexsAvailabletoMove(xPos, yPos, game.pieceSelected);
@@ -1207,20 +1209,59 @@ function selectPiece(p) {
                     game.pieceToMoveHexSelected = true;
 
                     updateMessageBox(xPos, yPos);
-                    return; //TODO 
-                } else {
-                    game.pieceSelected = 'none';
+                }
+
+            } else {
+                //check if hex to move to is valid
+
+                if (game.availableForMove(xPos, yPos) && game.availableCurrentPieceMove(xPos, yPos)) {
+                    //move piece
+                    game.movePiece(xPos, yPos, game.pieceSelected);
+
+                    //test if game won - if so exit and display winner
+                    if (game.isGameWon() == true) {
+                        displayWinnerMessage();
+                        game.gameOver = true;
+                        //TODO - update game record with final move?
+                        return;
+                    }
+
+                    game.readyToMove = false;
                     game.pieceToMoveHexSelected = false;
-                    game.colourSelected = 'none';
+                    game.changeSideToMove();
+                    game.resetAvailableMoves();
+                    game.oldX = 15;
+                    game.oldY = 40;
+                    updateMessageBox(xPos, yPos);
+                    return; //TODO
+
+                } else {
+                    game.resetAvailableMoves();
+                    //if hex not available unselect piece - select new piece if one in hex
+                    if (game.availableForSelection(xPos, yPos, game.sideToMove)) {
+                        game.resetAvailableMoves();
+                        game.selectHex(xPos, yPos, 'blue');
+                        game.setPieceSelected(xPos, yPos);
+                        game.showHexsAvailabletoMove(xPos, yPos, game.pieceSelected);
+                        game.oldX = game.selectedCellX;
+                        game.oldY = game.selectedCellY;
+                        game.readyToMove = true;
+                        game.pieceToMoveHexSelected = true;
+                        updateMessageBox(xPos, yPos);
+                        return; //TODO 
+                    } else {
+                        game.pieceSelected = 'none';
+                        game.pieceToMoveHexSelected = false;
+                        game.colourSelected = 'none';
+                    }
                 }
             }
         }
 
 
+    } else {
+        displayWinnerMessage();
     }
-
-
-
 } //end_selectPiece
 
 
